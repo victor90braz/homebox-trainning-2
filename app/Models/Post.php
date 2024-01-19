b966a7e8dc9a8261b002de6829970e399c2d85c8
victor braz <victor.90braz@gmail.com>
2024-01-19 09:36:11 +0100

    extract handle function filter

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-
     protected $guarded = [];
     protected $with = ['category', 'author'];
     public function scopeFilter($query, array $filters): void
@@ -19,11 +18,9 @@ class Post extends Model
                 ->orWhere('body', 'like', '%' . $search . '%')
         );
     }
-
     public function category() {
         return $this->belongsTo(Category::class);
     }
-
     public function author() {
         return $this->belongsTo(User::class, 'user_id');
     }
