aab46fb369a5274739c61a6c21f2f3705c4cb27b
victor braz <victor.90braz@gmail.com>
2024-01-19 08:51:15 +0100

    add a search request

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,20 @@ use App\Models\User;
 
 Route::get('/', function () {
 
+    //dd(request('search'));
+
+    $posts  = Post::latest();
+
+    if(request('search')) {
+        $posts
+            ->where('title', 'like', '%' . request('search' . '%'))
+            ->orWhere('body', 'like', '%' . request('search') . '%');
+    }
+
+
+
     return view('posts', [
-        'posts' => Post::latest()->get(),
+        'posts' => $posts->get(),
         'categories' => Category::all()
     ]);
 
